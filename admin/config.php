d3866e943a4bfcbdbc5243b707798d06e22d97e4
Nina Novoselska <napril96@gmail.com>
2016-11-19 21:38:25 +0200

    initialisation

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://cityexpress/admin/');
+define('HTTP_CATALOG', 'http://cityexpress/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://cityexpress/admin/');
+define('HTTPS_CATALOG', 'http://cityexpress/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/cityexpress/admin/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/cityexpress/system/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/cityexpress/admin/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/cityexpress/admin/view/template/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/cityexpress/system/config/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/cityexpress/image/');
+define('DIR_CACHE', 'C:/OpenServer/domains/cityexpress/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/cityexpress/system/storage/download/');
+define('DIR_LOGS', 'C:/OpenServer/domains/cityexpress/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OpenServer/domains/cityexpress/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OpenServer/domains/cityexpress/system/storage/upload/');
+define('DIR_CATALOG', 'C:/OpenServer/domains/cityexpress/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'cityexpress');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://cityexpress/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://cityexpress/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/OpenServer/domains/cityexpress/catalog/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/cityexpress/system/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/cityexpress/catalog/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/cityexpress/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/cityexpress/system/config/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/cityexpress/image/');
+define('DIR_CACHE', 'C:/OpenServer/domains/cityexpress/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/cityexpress/system/storage/download/');
+define('DIR_LOGS', 'C:/OpenServer/domains/cityexpress/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OpenServer/domains/cityexpress/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OpenServer/domains/cityexpress/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'cityexpress');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
