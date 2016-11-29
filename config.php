a4299edd1f47c25ed248b98a89267ddb0e8b62a1
Nina Novoselska <napril96@gmail.com>
2016-11-29 23:21:18 +0200

    add gitignore
    by Nina

--- config.php
+++ config.php
@@ -6,17 +6,17 @@ define('HTTP_SERVER', 'http://cityexpress/');
 define('HTTPS_SERVER', 'http://cityexpress/');
 
 // DIR
-define('DIR_APPLICATION', 'E:/OpenServer/domains/cityexpress/catalog/');
-define('DIR_SYSTEM', 'E:/OpenServer/domains/cityexpress/system/');
-define('DIR_LANGUAGE', 'E:/OpenServer/domains/cityexpress/catalog/language/');
-define('DIR_TEMPLATE', 'E:/OpenServer/domains/cityexpress/catalog/view/theme/');
-define('DIR_CONFIG', 'E:/OpenServer/domains/cityexpress/system/config/');
-define('DIR_IMAGE', 'E:/OpenServer/domains/cityexpress/image/');
-define('DIR_CACHE', 'E:/OpenServer/domains/cityexpress/system/storage/cache/');
-define('DIR_DOWNLOAD', 'E:/OpenServer/domains/cityexpress/system/storage/download/');
-define('DIR_LOGS', 'E:/OpenServer/domains/cityexpress/system/storage/logs/');
-define('DIR_MODIFICATION', 'E:/OpenServer/domains/cityexpress/system/storage/modification/');
-define('DIR_UPLOAD', 'E:/OpenServer/domains/cityexpress/system/storage/upload/');
+define('DIR_APPLICATION', 'C:/OpenServer/domains/cityexpress/catalog/');
+define('DIR_SYSTEM', 'C:/OpenServer/domains/cityexpress/system/');
+define('DIR_LANGUAGE', 'C:/OpenServer/domains/cityexpress/catalog/language/');
+define('DIR_TEMPLATE', 'C:/OpenServer/domains/cityexpress/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/OpenServer/domains/cityexpress/system/config/');
+define('DIR_IMAGE', 'C:/OpenServer/domains/cityexpress/image/');
+define('DIR_CACHE', 'C:/OpenServer/domains/cityexpress/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/OpenServer/domains/cityexpress/system/storage/download/');
+define('DIR_LOGS', 'C:/OpenServer/domains/cityexpress/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/OpenServer/domains/cityexpress/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/OpenServer/domains/cityexpress/system/storage/upload/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
