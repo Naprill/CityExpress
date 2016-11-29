b02d31d092282f20439177a947d345af8b098f9a
Nina Novoselska <napril96@gmail.com>
2016-11-29 23:11:20 +0200

    add ukrainian language and other changes,
    by Nina

---
+++ catalog/language/ukrainian/payment/bluepay_hosted.php
@@ -0,0 +1,28 @@
+<?php
+// Text
+$_['text_title']				= 'Кредитна / дебетова карта (BluePay)';
+$_['text_credit_card']			= 'Дані карти';
+$_['text_description']			= 'товарів в %s замовлення №: %s';
+$_['text_card_type']			= 'Тип карти: ';
+$_['text_card_name']			= 'Назва карти: ';
+$_['text_card_digits']			= 'Останні цифри: ';
+$_['text_card_expiry']			= 'Строк дії: ';
+$_['text_transaction_error']	= 'Сталася помилка при обробці транзакції - ';
+
+// Entry
+$_['entry_card']				= 'Нова або існуюча карта: ';
+$_['entry_card_existing']		= 'Існуюча';
+$_['entry_card_new']			= 'Нова';
+$_['entry_card_save']			= 'Запам’ятати дані карти';
+$_['entry_cc_owner']			= 'Власник карти';
+$_['entry_cc_number']			= 'Номер карти';
+$_['entry_cc_start_date']		= 'Карта діє з дати';
+$_['entry_cc_expire_date']		= 'Строк дії карти';
+$_['entry_cc_cvv2']				= 'Код безпеки карти (CVV2)';
+$_['entry_cc_address']			= 'Адреса вулиці';
+$_['entry_cc_city']				= 'Місто';
+$_['entry_cc_state']			= 'Область';
+$_['entry_cc_zipcode']			= 'Поштовий індекс';
+$_['entry_cc_phone']			= 'Телефон';
+$_['entry_cc_email']			= 'Адреса електронної пошти';
+$_['entry_cc_choice']			= 'Вибір існуючої карти';
